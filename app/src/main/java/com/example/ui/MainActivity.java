65d8f823a5f3334f19b2d4c25ce540552d2e54e2
wudgocks <junghc0304@naver.com>
2022-11-09 21:50:36 +0900

    Initial commit

---
+++ app/src/main/java/com/example/ui/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bottom_menu);
+
+    }
+}
